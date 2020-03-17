6cf2d425ae2eb27296a331674281598ac288f7bd
blody09 <blakeody9@gmail.com>
2020-03-17 19:25:57 -0400

    fixed Error in the YourProfile action that was that was not taking the UserId and worked on the Profile View, also patched up the searchgame view to redirect to displaygames

---
+++ GCBlueTeamFinalProject/GCBlueTeamFinalProject/Models/UsersGamers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GCBlueTeamFinalProject.Models
+{
+    public class UsersGamers
+    {
+        public Users MyUser { get; set; }
+        public Gamers MyGamer { get; set; }
+
+        public UsersGamers(Users myUser, Gamers myGamer)
+        {
+            MyUser = myUser;
+            MyGamer = myGamer;
+
+        }
+       
+    }
+
+
+
+}
